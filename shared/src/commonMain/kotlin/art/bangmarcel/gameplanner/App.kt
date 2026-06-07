2c71e23e981d9e63ba04fc51bf318cc75e395b29
MarcelArt <badman.islemon@gmail.com>
2026-06-07 16:04:30 +0700

    style - update theme

--- shared/src/commonMain/kotlin/art/bangmarcel/gameplanner/App.kt
+++ shared/src/commonMain/kotlin/art/bangmarcel/gameplanner/App.kt
@@ -1,36 +1,20 @@
 package art.bangmarcel.gameplanner
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import art.bangmarcel.gameplanner.configs.initFileKit
 import art.bangmarcel.gameplanner.database.createDatabase
 import art.bangmarcel.gameplanner.repositories.GameRepo
 import art.bangmarcel.gameplanner.screens.GameListScreen
-import art.bangmarcel.gameplanner.theme.GamePlannerTheme
-import art.bangmarcel.gameplanner.viewmodels.GameListViewModel
 import cafe.adriel.voyager.navigator.Navigator
-import org.jetbrains.compose.resources.painterResource
-
-import gameplanner.shared.generated.resources.Res
-import gameplanner.shared.generated.resources.compose_multiplatform
+import art.bangmarcel.gameplanner.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
     initFileKit()
-    GamePlannerTheme {
+    AppTheme {
         val db = remember { createDatabase() }
         val gRepo = remember { GameRepo(db.gameDao()) }
 
